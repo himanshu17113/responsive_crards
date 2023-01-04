803690f7e72a894b648a346f29a6737cebf4d36f
himanshu17113 <98476250+himanshu17113@users.noreply.github.com>
2023-01-04 19:32:09 +0530

    final

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({
     super.key,
@@ -15,11 +16,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Flexible(
+              Padding(
+                padding: const EdgeInsets.only(top: 70, bottom: 80),
                 child: Container(
-                  //width: 405,
                   height: 125,
                   child: Card(
                     color: Color.fromRGBO(209, 133, 133, 1),
@@ -27,97 +28,73 @@ class _MyHomePageState extends State<MyHomePage> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
-                          Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 8.0, right: 8.0),
-                                child: Flexible(
-                                    child: Container(
-                                  color: Color.fromRGBO(196, 196, 196, 1),
-                                  width: 162,
-                                  height: 20,
-                                )),
-                              ),
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 8.0,
+                              right: 8.0,
+                            ),
+                            child: Container(
+                              color: Color.fromRGBO(196, 196, 196, 1),
+                              width: 162,
+                              height: 20,
+                            ),
                           ),
-                          Row(
-                            children: [
-                              Flexible(
-                                  //flex: 18,
-                                  child: Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 8.0, right: 22.0),
-                                child: Container(
-                                  color: Color.fromRGBO(168, 216, 173, 1),
-                                  //width: 162,
-                                  height: 30,
-                                ),
-                              )),
-                            ],
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 8.0, right: 22.0),
+                            child: Container(
+                              color: Color.fromRGBO(168, 216, 173, 1),
+                              height: 30,
+                            ),
                           )
                         ]),
                   ),
                 ),
               ),
-              Flexible(
-                child: Stack(
-                    // alignment: Alignment.topCenter,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 150,
-                      ),
-                      // Container(color: Colors.black, height: 150),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 25.0),
-                        child: Container(
-                          //width: 405,
-                          height: 125,
-                          child: Card(
-                            color: Color.fromRGBO(209, 133, 133, 1),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  Row(
-                                    children: [
-                                      Flexible(
-                                          //flex: 18,
-                                          child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 8.0, right: 8.0, top: 26),
-                                        child: Container(
-                                          color:
-                                              Color.fromRGBO(168, 216, 173, 1),
-                                          //width: 162,
-                                          height: 30,
-                                        ),
-                                      )),
-                                    ],
+              Stack(
+                  // alignment: Alignment.topCenter,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 150,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25.0),
+                      child: Container(
+                        //width: 405,
+                        height: 125,
+                        child: Card(
+                          color: Color.fromRGBO(209, 133, 133, 1),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 8.0, right: 8.0, top: 26),
+                                  child: Container(
+                                    color: Color.fromRGBO(168, 216, 173, 1),
+                                    //width: 162,
+                                    height: 30,
                                   ),
-                                ]),
-                          ),
+                                ),
+                              ]),
                         ),
                       ),
-                      //  SizedBox(height: 50),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 8.0, right: 8.0),
-                            child: Flexible(
-                                child: Container(
-                              color: Color.fromRGBO(196, 196, 196, 1),
-                              width: 324,
-                              height: 50,
-                            )),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                          child: Container(
+                            color: Color.fromRGBO(196, 196, 196, 1),
+                            width: 284,
+                            height: 50,
                           ),
-                        ],
-                      ),
-                    ]),
-              )
+                        ),
+                      ],
+                    ),
+                  ]),
             ],
           ),
         ),
