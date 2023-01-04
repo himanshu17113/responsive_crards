abef9365930f84d810cf45e47459e3688ed82fb4
himanshu17113 <98476250+himanshu17113@users.noreply.github.com>
2023-01-04 18:57:09 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -11,96 +11,116 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var size, height, width;
-
   @override
   Widget build(BuildContext context) {
-    size = MediaQuery.of(context).size;
-    height = size.height;
-    width = size.width;
-    print(height);
-    print(width);
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: resposivelayouts(
-            mobile: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  child: Container(
-                    //width: 405,
-                    height: 150,
-                    child: Card(
-                      color: Color.fromRGBO(209, 133, 133, 1),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 8.0, right: 8.0),
-                                  child: Flexible(
-                                      child: Container(
-                                    color: Color.fromRGBO(196, 196, 196, 1),
-                                    width: 162,
-                                    height: 20,
-                                  )),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Flexible(
-                                    //flex: 18,
-                                    child: Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 8.0, right: 22.0),
-                                  child: Container(
-                                    color: Color.fromRGBO(168, 216, 173, 1),
-                                    //width: 162,
-                                    height: 30,
-                                  ),
-                                )),
-                              ],
-                            )
-                          ]),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            tab: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  child: Container(
-                    //width: 405,
-                    height: 150,
-                    child: Card(
-                      color: Color.fromRGBO(209, 133, 133, 1),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            Row(
-                              children: [
-                                Flexible(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Flexible(
+                child: Container(
+                  //width: 405,
+                  height: 125,
+                  child: Card(
+                    color: Color.fromRGBO(209, 133, 133, 1),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8.0, right: 8.0),
+                                child: Flexible(
                                     child: Container(
-                                  color: Colors.blueGrey,
+                                  color: Color.fromRGBO(196, 196, 196, 1),
                                   width: 162,
                                   height: 20,
                                 )),
-                              ],
-                            )
-                          ]),
-                    ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Flexible(
+                                  //flex: 18,
+                                  child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8.0, right: 22.0),
+                                child: Container(
+                                  color: Color.fromRGBO(168, 216, 173, 1),
+                                  //width: 162,
+                                  height: 30,
+                                ),
+                              )),
+                            ],
+                          )
+                        ]),
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              Flexible(
+                child: Stack(
+                    // alignment: Alignment.topCenter,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 150,
+                      ),
+                      // Container(color: Colors.black, height: 150),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 25.0),
+                        child: Container(
+                          //width: 405,
+                          height: 125,
+                          child: Card(
+                            color: Color.fromRGBO(209, 133, 133, 1),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Row(
+                                    children: [
+                                      Flexible(
+                                          //flex: 18,
+                                          child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 8.0, right: 8.0, top: 26),
+                                        child: Container(
+                                          color:
+                                              Color.fromRGBO(168, 216, 173, 1),
+                                          //width: 162,
+                                          height: 30,
+                                        ),
+                                      )),
+                                    ],
+                                  ),
+                                ]),
+                          ),
+                        ),
+                      ),
+                      //  SizedBox(height: 50),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 8.0, right: 8.0),
+                            child: Flexible(
+                                child: Container(
+                              color: Color.fromRGBO(196, 196, 196, 1),
+                              width: 162,
+                              height: 50,
+                            )),
+                          ),
+                        ],
+                      ),
+                    ]),
+              )
+            ],
           ),
         ),
       ),
